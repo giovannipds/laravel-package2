77e8826aafc66a58407345d6505af14ffb747069
Giovanni Pires da Silva <giovanni@somosgad.com>
2019-12-10 14:46:10 -0300

    Update the config file

--- packages/Acme/PageReview/config/pagereview.php
+++ packages/Acme/PageReview/config/pagereview.php
@@ -1,5 +1,12 @@
 <?php
+// File: packages/Acme/PageReview/config/pagereview.php
 
 return [
-    //
+    /*
+    * This determines how the reviews will be ordered when fetched
+    */
+    'order' => [
+        'by' => 'DESC',
+        'as' => 'created_at',
+    ],
 ];
